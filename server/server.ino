a2d2f08092aa0a3961282b40a893cb4308f35f11
HankRobot <thc1n17@soton.ac.uk>
2019-06-26 14:48:58 +0800

    simplified server troubleshooting code. testing

--- server/server.ino
+++ server/server.ino
@@ -68,15 +68,15 @@ void loop()
     if (rf95.recv(buf, &len))
     {
       Serial.print("Got: ");
-      Serial.println((char*)buf);
-       Serial.print("RSSI: ");
-      Serial.println(rf95.lastRssi(), DEC);
+      Serial.print((char*)buf);
+      Serial.print(" RSSI: ");
+      Serial.print(rf95.lastRssi(), DEC);
 
       // Send a reply
       uint8_t data[] = "And hello back to you";
       rf95.send(data, sizeof(data));
       rf95.waitPacketSent();
-      Serial.println("Sent a reply");
+      Serial.println(" Sent a reply");
     }
     else
     {
