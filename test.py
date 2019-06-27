29f01168085f812042fdf3e3f1611c394518b956
HankRobot <thc1n17@soton.ac.uk>
2019-06-27 16:44:30 +0800

    created python script to generate private key

--- test.py
+++ test.py
@@ -1,8 +1,19 @@
 import subprocess
 import codecs
+import hashlib
+
+serialID = "1234567"
 
 print("Please enter your password")
 password = input()
-s = subprocess.check_output(["node","transfer.js",str(password)])
-s = codecs.decode(s,'unicode_escape')
-print(s)
+bpass = str.encode(serialID+password)
+privatekey = hashlib.sha3_256(bpass)
+print(privatekey.hexdigest())
+
+
+#s = subprocess.check_output(["node","transfer.js",str(password)])
+#s = codecs.decode(s,'unicode_escape')
+#print(s)
+
+
+
