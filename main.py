69b4691bfeb419cc03d351739c29b3cfac41bb94
HankRobot <thc1n17@soton.ac.uk>
2019-06-27 16:35:44 +0800

    fixed another bug on check message

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ ser = serial.Serial('/dev/ttyUSB0',9600)     #connection to the Arduino
 
 while True:
     message=ser.readline()
-    message = codecs.decode(s,'unicode_escape')
-    print (message))
-    if(message[0:3]=='Got:'):
+    message = codecs.decode(message,'unicode_escape')
+    print (message)
+    if(message[0:3]=='Got'):
         print("Please enter your password")
         password = input()
-        s = subprocess.check.output(["node","transfer.js"+ str(password)])
+        s = subprocess.check_output(["node","transfer.js",str(password)])
         s = codecs.decode(s,'unicode_escape')
         print(s)
     
