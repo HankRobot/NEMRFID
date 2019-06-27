492eb09819e5c356acdd48551d8411e43c8956bb
HankRobot <thc1n17@soton.ac.uk>
2019-06-27 17:13:47 +0800

    migrated test to main

--- main.py
+++ main.py
@@ -9,9 +9,15 @@ while True:
     message = codecs.decode(message,'unicode_escape')
     print (message)
     if(message[0:3]=='Got'):
+        serialID = "1234567"
+
         print("Please enter your password")
         password = input()
-        s = subprocess.check_output(["node","transfer.js",str(password)])
+        bpass = str.encode(serialID+password)
+        privatekey = hashlib.sha3_256(bpass).hexdigest()
+        print(privatekey)
+
+        s = subprocess.check_output(["node","transfer.js",privatekey])
         s = codecs.decode(s,'unicode_escape')
         print(s)
     
