d363340c908a808abf9671e623205d7f93839216
HankRobot <thc1n17@soton.ac.uk>
2019-06-27 15:55:06 +0800

    changed naming and update serial to working script

---
+++ main.py
@@ -0,0 +1,13 @@
+import serial
+import subprocess
+
+ser = serial.Serial('/dev/ttyUSB0',9600)     #connection to the Arduino
+
+while True:
+    message=ser.readline()
+    print (got_serial)
+    if(message[0:4]==b'Got:'):
+        print("Please enter your password")
+        password = input()
+        s = subprocess.check.output(["node","transfer.js"+ str(password)])
+    
