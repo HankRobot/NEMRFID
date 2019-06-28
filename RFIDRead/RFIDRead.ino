13f2d30737966bf7c34cf697a01b7051873776b7
HankRobot <thc1n17@soton.ac.uk>
2019-06-28 15:22:35 +0800

    added multi sig

--- RFIDRead/RFIDRead.ino
+++ RFIDRead/RFIDRead.ino
@@ -21,8 +21,8 @@ void setup()
   mfrc522.PCD_Init();   // Initiate MFRC522
   Serial.println("Approximate your card to the reader...");
   Serial.println();
-
 }
+
 void loop() 
 {
   // Look for new cards
@@ -39,24 +39,22 @@ void loop()
   Serial.print("UID tag :");
   String content= "";
   byte letter;
-  for (byte i = 0; i < mfrc522.uid.size; i++) 
-  {
-     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
-     Serial.print(mfrc522.uid.uidByte[i], HEX);
-     content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
-     content.concat(String(mfrc522.uid.uidByte[i], HEX));
+  for (byte i = 0; i < mfrc522.uid.size; i++){
+    Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+    Serial.print(mfrc522.uid.uidByte[i], HEX);
+    content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+    content.concat(String(mfrc522.uid.uidByte[i], HEX));
   }
   Serial.println();
   Serial.print("Message : ");
   content.toUpperCase();
-  if (content.substring(1) == "BD 31 15 2B") //change here the UID of the card/cards that you want to give access
-  {
+  Serial.print(content.substring(1));
+  if (content.substring(1) == "BD 31 15 2B"){ //change here the UID of the card/cards that you want to give access
     Serial.println("Authorized access");
     Serial.println();
     delay(3000);
   }
- 
- else   {
+  else{
     Serial.println(" Access denied");
     delay(3000);
   }
